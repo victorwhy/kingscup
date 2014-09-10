b04d3bdd0d8b21e531c9de7a6595af2170e80e23
Victor Yang <victory@Victors-MacBook-Pro.local>
2014-09-10 18:41:58 -0400

    Create routes for showing past game

--- app/controllers/game.rb
+++ app/controllers/game.rb
@@ -1,3 +1,24 @@
 get '/game' do
   erb :game
 end
+
+post '/game/:id' do
+  game = Game.find(params[:id])
+  players = game.players
+  @game_object = players.each_with_object({}) do |player, game_hash|
+    game_hash[player.name] = player.cards
+  end
+  @game_object.to_json
+end
+
+# players.each_with_object {} do |player, game_object|
+#   game_object << {(player.name): player.cards}
+# end.to_json
+
+# arr = []
+# collection.each do |x|
+#   arr << x
+# end
+# arr
+
+# collection.each_with_object [] { |x, arr| arr << x }.to_json
